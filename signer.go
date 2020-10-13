61476a96ab40ee17cc9b82cb7da2a81fcbd6ede9
Lavrov Aleksandr <lismut@gmail.com>
2020-10-13 22:55:21 +0300

    drafts

--- signer.go
+++ signer.go
@@ -1,3 +1,18 @@
 package main
 
 // сюда писать код
+func SingleHash(data string) string {
+	return "aa"
+}
+
+func MultiHash () {
+
+}
+
+func CombineResults() {
+	
+}
+
+func ExecutePipeline( param []job) {
+
+}
